c1f78f8d56b0bbef2313acfe368bbd5de7e95e22
Marius Gundersen <me@mariusgundersen.net>
2014-04-13 17:24:23 +0200

    images proxied via the server to the client. Started working on clockwise spiral loop

---
+++ getImage.js
@@ -0,0 +1,10 @@
+var request = require('request');
+
+module.exports = function(uri, stream){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(stream);
+  });
+};
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var connect = require('connect');
 var getAvailableRadar = require('./getAvailableRadar');
+var getImage = require('./getImage');
+var urlrouter = require('urlrouter');
 
 
 connect()
-.use(function(req, res, next){
-  if(req.url != "/available"){
-    next();
-    return;
-  }
-  getAvailableRadar(function(err, data){
-    res.end(JSON.stringify(data));
+.use(urlrouter(function(app){
+  app.get('/available', function(req, res, next){
+    getAvailableRadar(function(err, data){
+      res.end(JSON.stringify(data));
+    });
+  });
+  app.get('/image/', function(req, res, next){
+    console.dir(req);
+    getImage('http://api.yr.no/weatherapi/radar/1.4/' + req._parsedUrl.search, res);
   });
   
-  
-})
+}))
 .use(connect.static(__dirname)).listen(80);
