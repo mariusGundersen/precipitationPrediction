e0d77e83bf8f13f4c23e599bbda3adea115d91b8
Marius Gundersen <me@mariusgundersen.net>
2014-06-22 14:32:42 +0200

    Added linear best fit algorithm of the distance to first rain

---
+++ frontend/js/linearBestFit.js
@@ -0,0 +1,62 @@
+define(function(){
+  
+  return function(values){
+    
+    var n = values.length;
+    
+    var x = values.map(function(entry){
+      return entry.x;
+    });
+    
+    var y = values.map(function(entry){
+      return entry.y;
+    });
+    
+    var xy = values.map(function(entry){
+      return entry.x * entry.y;
+    });
+    
+    var x2 = x.map(function(x){
+      return x*x;
+    });
+    
+    var sumX = sum(x);
+    
+    var sumY = sum(y);
+    
+    var sumXY = sum(xy);
+    
+    var sumX2 = sum(x2);
+    
+    var m = (sumXY - sumX*sumY/n)/(sumX2 - sumX*sumX/n);
+    
+    var avgX = sumX/n;
+    
+    var avgY = sumY/n;
+    
+    var b = avgY - m*avgX;
+    
+    var e = values.map(function(entry){
+      return entry.y - (b + entry.x*m);
+    });
+    
+    var e2 = e.map(function(e){
+      return e*e;
+    });
+    
+    var sumE2 = sum(e2);
+        
+    return {
+      m: m,
+      b: b,
+      e: sumE2
+    };
+  };
+  
+  function sum(values){
+    return values.reduce(function(a, b){
+      return a + b; 
+    }, 0);
+  }
+  
+});
